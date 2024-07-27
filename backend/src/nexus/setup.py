5bbd4dd4de215dce0923d62b40a417cb8f242e0c
Felipe Macias <felipe.macias@gmail.com>
2024-07-27 06:08:31 -0600

    grupos keycloak

--- backend/src/nexus/setup.py
+++ backend/src/nexus/setup.py
@@ -46,6 +46,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "pas.plugins.oidc",
+        "pas.plugins.keycloakgroups",
         "setuptools",
         "Plone",
         "prettyconf",
